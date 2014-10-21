aa94763b6abf464d1b62c4281f7c6ee76aa80759
goldie038 <chrysso@teiath.gr>
2014-10-21 21:19:48 +0300

    Hope all done well

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-07 16:47
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = 'Goldie038#2011';
+$mysqlMainDb = 'eclass';
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
+
